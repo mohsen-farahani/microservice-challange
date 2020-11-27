950d41140da271a9499eccdcefb2380ad4bd11ec
Mohsen <civil.farahani@gmail.com>
2020-11-27 16:50:23 +0330

    dockerize gatewai-api service

--- apps/gateway-api/index.js
+++ apps/gateway-api/index.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("Simple API Gateway")
+    res.send("API Gateway")
 })
 
 app.use(router)
 
-console.log("Simple API Gateway run on localhost:3000")
+console.log("API Gateway run on localhost:3000")
 
 app.listen(3000);
